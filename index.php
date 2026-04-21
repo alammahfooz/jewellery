1e01e6eb1381cc92c5e6c618ada4c0368b7a8009
Mahfooz Alam <alammahfooz53356@gmail.com>
2026-04-22 04:54:29 +0530

    update

--- index.php
+++ index.php
@@ -530,7 +530,7 @@ if (isset($_POST['subcategory'])) {
             <div class="col-lg-12">
                 <div class="title-area-between">
                     <h2 class="title-left mb--0">
-                        Latest Blog Post Insights
+                        Latest Blog Post Insights Mahfooz
                     </h2>
                 </div>
             </div>
